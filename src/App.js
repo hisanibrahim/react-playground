2cefacbb9549069f8cf5d9e8996ca9677efef0e9
Hisan Ibrahim <hisanibrahim@gmail.com>
2023-09-03 18:10:31 +0530

    Code splitting implementation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
+import { Suspense, lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 
 import Layout from "./components/Layout";
 import ErrorPage from "./pages/ErrorPage";
-import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Users from "./pages/Users";
 import Home from "./pages/Home";
 import RequireAuth from "./components/RequireAuth";
 import { AuthProvider } from "./features/auth/context";
+const Login = lazy(() => import("./pages/Login"));
 
 function App() {
   const defaultTheme = createTheme();
@@ -21,7 +22,14 @@ function App() {
           <Routes>
             <Route element={<Layout />}>
               <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
+              <Route
+                path="/login"
+                element={
+                  <Suspense>
+                    <Login />
+                  </Suspense>
+                }
+              />
               <Route path="/register" element={<Register />} />
               <Route
                 path="/users"
@@ -32,6 +40,7 @@ function App() {
                 }
               />
             </Route>
+            <Route path="*" element={<ErrorPage />} />
           </Routes>
         </ThemeProvider>
       </AuthProvider>
